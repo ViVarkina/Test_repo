c0c49d351af319033fcf7589a733b956825f90f3
ViVarkina <vikavarkina@gmail.com>
2024-02-10 19:26:48 +0300

    lesson7

--- Task/isPasswordStrong/isPasswordStrong.js
+++ Task/isPasswordStrong/isPasswordStrong.js
@@ -1,18 +1,1 @@
-//8,9
-
-function isPasswordStrong(password){
-<<<<<<< HEAD
-    lebg = 0
-    for (let i = 0; i < password; i++) {
-        console.log(password.length)
-
-    }
-}
-
-isPasswordStrong("Чтото")
-=======
-    if (length(password) >= 6) {
-        return 
-    }
-}
->>>>>>> origin/master
+//8 9
