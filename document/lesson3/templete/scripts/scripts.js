08572d9d2daf9c99192c43a03eecb7c8a7570b82
ViVarkina <vikavarkina@gmail.com>
2024-03-31 08:33:01 +0300

    24.03.24

--- document/lesson3/templete/scripts/scripts.js
+++ document/lesson3/templete/scripts/scripts.js
@@ -36,6 +36,9 @@ document.getElementById("formContainer").append(createButton())
 
 const addEventListenerForButton=()=>{
     const btn =  document.getElementById("login")
+    const inputFirstNam = document.getElementById("firstName")
+    const inputLastName = document.getElementById("lastName")
+    const inputPassword = document.getElementById("password")
     const  onClickHandler=(event)=>{
         console.log("Клик", event)
         const inputList = document.getElementsByTagName("input")
@@ -60,9 +63,29 @@ const addEventListenerForButton=()=>{
         event.target.classList.remove("btnLoginMouseOver")
         // console.log("Покинул кнопку")
     }
+    const  onFocus=(event)=>{
+        console.log(event.target)
+        event.target.classList.add("inputLoginFocus")
+        // console.log("На кнопке при нажатии", event)
+    }
+    const  onFocusLeave=(event)=>{
+        event.target.classList.remove("inputLoginFocus")
+        // console.log("Покинул кнопку", event)
+    }
+
+
+
     btn.addEventListener("click", onClickHandler) //при клике
     btn.addEventListener("mouseover", onMouseoverHandler)//при наведении
     btn.addEventListener("mouseleave", onMouseoverLeave)//покинул
-    //focus- попадание меняет цвет 
+    inputFirstNam.addEventListener("focus",onFocus)
+    inputFirstNam.addEventListener("blur", onFocusLeave)
+
+    inputLastName.addEventListener("focus",onFocus)
+    inputLastName.addEventListener("blur", onFocusLeave)
+
+    inputPassword.addEventListener("focus",onFocus)
+    inputPassword.addEventListener("blur", onFocusLeave)
+    //focus- попадание меняет цвет
 }
 addEventListenerForButton()
